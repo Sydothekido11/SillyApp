3b0d6ea94aae1f4b3f7d573d4ec2191e9332b022
Sydothekido11 <sydnemelka@gmail.com>
2015-12-16 13:26:00 -0700

    Made an app that changes colors when a button is pressed.

--- SillyApp/ViewController.swift
+++ SillyApp/ViewController.swift
@@ -8,14 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+  
+    @IBAction func changeRedColor()
+    {
+        view.backgroundColor = UIColor.redColor()
+    }
+    
+    @IBAction func changeBlueColor()
+    {
+        view.backgroundColor = UIColor.blueColor()
+    }
+    
+    @IBAction func changeYellowButton()
+    {
+        view.backgroundColor = UIColor.yellowColor()
+    }
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
